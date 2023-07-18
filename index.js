6bcf0ba2026493b80edc158f54255b0d4a0cbcb8
Anna Muzalevskaya <77303782+NitaLewska@users.noreply.github.com>
2023-07-19 00:20:34 +0300

    feat: add functionality to technology page

--- index.js
+++ index.js
@@ -58,10 +58,9 @@ buttonsCrew.forEach(a => a.addEventListener('click', function() {
 }))
 
 const buttonsTech = document.querySelectorAll('.grid-container--technology>.number-indicators>button')
-// const crewHeading = document.querySelector('.crew-details h2')
-// const crewName = document.querySelector('.crew-details h2 + p')
+const techName = document.querySelector('.technology-details h2 + p')
 const techDescription = document.querySelector('.technology-details>p')
-// const crewImages = document.querySelectorAll(".grid-container--crew>picture")
+const techImages = document.querySelectorAll(".grid-container--technology>picture")
 
 
 
@@ -70,11 +69,10 @@ buttonsTech.forEach(a => a.addEventListener('click', function() {
     buttonsTech.forEach(b => b.ariaSelected = 'false')
     a.ariaSelected = 'true'
     let index = a.innerHTML - 1
-    // crewHeading.innerHTML = data.crew[index].role
     techDescription.innerHTML = data.technology[index].description
-    // crewName.innerHTML = data.crew[index].name
-    // crewImages.forEach(i => i.setAttribute('style', 'display:none'))
-    // let currentImage = document.querySelector(`.grid-container--crew>picture:nth-of-type(${index + 1})`)
-    // currentImage.setAttribute('style', 'display:block')
+    techName.innerHTML = data.technology[index].name
+    techImages.forEach(i => i.setAttribute('style', 'display:none'))
+    let currentImage = document.querySelector(`.grid-container--technology>picture:nth-of-type(${index + 1})`)
+    currentImage.setAttribute('style', 'display:block')
 }))
 
